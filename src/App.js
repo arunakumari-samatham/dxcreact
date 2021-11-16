da113130389727d0cb5c825e803e02d3907ed2cc
arunakumari-samatham <arunakumariists@gmail.com>
2021-11-16 20:01:47 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import ChildComponent from './components/ChildComponent';
 import Fragments from './components/Fragments';
 import NewTable from './components/NewTable';
 import ClassClick from './components/ClassClick';
+import Counter from './components/Counter';
+import EventBind from './components/EventBind';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <ClassClick/>
+      <Form/>
     </div>
   );
 }
---
+++ src/components/EventBind.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class EventBind extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            message: 'Hello'
+        }
+       // this.clickHandler = this.clickHandler.bind(this)
+    }
+    //var a = 10
+    clickHandler = () => {
+        this.setState({
+            message: 'good night'
+        })
+    }
+
+   /*  clickHandler() {
+        console.log(this)
+        this.setState({
+            message: 'good bye'
+        })
+    } */
+
+    render() {
+        //using bind im tying clickhandler to this keyword -- binding in render method
+        return (
+            <div>
+                <div>{this.state.message}</div>
+                { /*<button onClick = {this.clickHandler.bind(this)}>click</button>  */}
+                {/* <button onClick = {() => this.clickHandler()}>click</button>   */}
+                <button onClick={this.clickHandler}>click</button>
+            </div>
+        )
+    }
+}
+
+export default EventBind
