73c695672e932021602c38512044212dab2bd8b4
arunakumari-samatham <arunakumariists@gmail.com>
2021-11-16 20:12:30 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import ClassClick from './components/ClassClick';
 import Counter from './components/Counter';
 import EventBind from './components/EventBind';
 import Form from './components/Form';
+import FunctionClick from './components/FunctionClick';
+import Inline from './components/Inline';
+import MemoComp from './components/MemoComp';
+import Message from './components/Message';
+import NameList from './components/NameList';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+      <ParentComponent/>
     </div>
   );
 }
