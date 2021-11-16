d36875f7c38bf464495bc39121d22cceb99af74e
arunakumari-samatham <arunakumariists@gmail.com>
2021-11-16 13:12:13 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Greet from './components/Greet';
 import ChildComponent from './components/ChildComponent';
 import Fragments from './components/Fragments';
 import NewTable from './components/NewTable';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
-      <NewTable/>
+      <ClassClick/>
     </div>
   );
 }
